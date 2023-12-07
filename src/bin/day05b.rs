66213205655a7861b644c28c86aa942dad733d9b
Legendarymarvin <mail@metz.dev>
2023-12-07 21:41:11 +0100

    day05, kinda

--- src/bin/day05b.rs
+++ src/bin/day05b.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use rayon::prelude::*;
 use std::hash::Hash;
 use regex::Regex;
 use itertools::Itertools;
@@ -27,7 +28,6 @@ fn solve(p0: &str, p2: bool) -> (i64, i64) {
     let mut lines = p0.lines();
     let (_, seeds) = lines.next().unwrap_or("").split_once(": ").unwrap();
     let seed_values: Vec<i64> = seeds.split(" ").into_iter().map(|v| v.parse::<i64>().unwrap()).collect();
-    let mut ranges = vec![];
     let mappings: BTreeMap<Relations, &str> = BTreeMap::from([(SeedToSoil, "seed-to-soil"), (SoilToFertilizer, "soil-to-fertilizer"),
         (FertilizerToWater, "fertilizer-to-water"), (WaterToLight, "water-to-light"), (LightToTemperature, "light-to-temperature"),
         (TemperatureToHumidity, "temperature-to-humidity"), (HumidityToLocation, "humidity-to-location")]);
@@ -60,76 +60,36 @@ fn solve(p0: &str, p2: bool) -> (i64, i64) {
     }
 
     let mut lowest_location = i64::MAX;
-    let mut mappings: BTreeMap<Relations, Vec<((i64, i64), (i64, i64))>> = BTreeMap::new();
-    dbg!(&maps);
     if p2 {
-        // this part two isn't working, but debugging it seems like a pain
-        // so instead I just threw multithreading at bruteforce in day05b
-        // seemed like the right thing to do.
+        let mut ranges = vec![];
         for (i, seed) in seed_values.iter().enumerate() {
             if i % 2 != 0 {
                 continue;
             }
-            ranges.push((*seed, seed + seed_values[i + 1] - 1));
+            let range = seed_values[i + 1];
+            ranges.push((*seed, *seed + range - 1));
         }
-        let mut to_check = ranges.clone();
-        dbg!(&to_check);
-        for (key, map) in maps.iter() {
-            let mut range_maps = vec![];
-            for range in &to_check {
-                for (source, destination) in map.iter() {
-                    let step = destination.0 - source.0;
-                    if (range.0 < source.0 && range.1 < source.0) || (range.0 > source.1 && range.1 > source.1) {
-                        continue;
-                    } else if range.0 >= source.0 && range.1 <= source.1 {
-                        // complete inside, complete map
-                        range_maps.push((range.clone(), (range.0 + step, range.1 + step)));
-                    } else if range.0 < source.0 && range.1 > source.1 {
-                        // envelope completely => only mapper inner
-                        range_maps.push((source.clone(), destination.clone()));
-                    } else if range.0 >= source.0 && range.1 > source.1 {
-                        // starts within source, but goes outside
-                        range_maps.push(((range.0, source.1), (range.0 + step, source.1 + step)));
-                    } else if range.0 < source.0 && range.1 <= source.1 {
-                        // starts before and goes within
-                        range_maps.push(((source.0, range.1), (source.0 + step, range.1 + step)));
+        dbg!(&ranges);
+        // brute force best force, took 5hrs with 12 cores. Perfectly reasonable.
+        lowest_location = (0..i64::MAX).into_par_iter().find_first(|&i|  {
+            let mut value = i;
+            dbg!(value);
+            'map: for (key, map) in maps.iter().rev() {
+                for (source_range, destination_range) in map.iter() {
+                    if is_in_range(value, destination_range) {
+                        let step = destination_range.0 - source_range.0;
+                        value = value - step;
+                        continue 'map;
                     }
                 }
             }
-            dbg!(&key, &range_maps);
-            'current_range: for range in &to_check {
-                let mut remaining = range.clone();
-                let mapped_ranges = range_maps.iter().cloned().map(|map| map.0).sorted().collect::<Vec<(i64, i64)>>();
-                for mapped in mapped_ranges {
-                    if remaining.0 >= mapped.0 && remaining.1 <= mapped.1 {
-                        // completely mapped, ignore it.
-                        continue 'current_range;
-                    }
-                    if remaining.0 < mapped.0 && remaining.1 > mapped.0 && remaining.1 < mapped.1 {
-                        // second part mapped, continue checking first
-                        remaining = (remaining.0, mapped.0 - 1);
-                        continue;
-                    }
-                    if remaining.0 > mapped.0 && remaining.0 < mapped.1 && remaining.1 > mapped.1 {
-                        // first part mapped, continue checking second
-                        remaining = (mapped.1 + 1, remaining.1);
-                        continue;
-                    }
+            for range in &ranges {
+                if is_in_range(value, range) {
+                    return true;
                 }
-                range_maps.push((remaining.clone(), remaining.clone()));
             }
-            dbg!(&range_maps);
-            mappings.insert(*key, range_maps.clone());
-            to_check = range_maps.iter().cloned().map(|map| map.1).sorted().collect::<Vec<(i64, i64)>>();
-        }
-        let x = mappings.get(&HumidityToLocation).unwrap();
-        for temp in x {
-            // to low 29861279, too high 100644325
-            if temp.1.0 == 0 || temp.1.0 == 29861279 {
-                continue;
-            }
-            lowest_location = i64::min(lowest_location, temp.1.0);
-        }
+            return false;
+        }).unwrap();
     } else {
         for seed in seed_values {
             let mut current = seed;
@@ -146,6 +106,10 @@ fn solve(p0: &str, p2: bool) -> (i64, i64) {
     }
 
     (lowest_location, 42)
+}
+
+fn is_in_range(p0: i64, p1: &(i64, i64)) -> bool {
+    p0 >= p1.0 && p0 <= p1.1
 }
 
 #[cfg(test)]
